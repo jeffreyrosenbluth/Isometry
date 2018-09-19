59206c4c24249f5b5afd381ce02b53f8630c19e1
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2018-09-19 18:40:08 -0400

    Initial Commit

---
+++ Isometry/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Isometry
+//
+//  Created by Jeffrey Rosenbluth on 9/19/18.
+//  Copyright © 2018 Applause Code. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
---
+++ Isometry/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Isometry
+//
+//  Created by Jeffrey Rosenbluth on 9/19/18.
+//  Copyright © 2018 Applause Code. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class ViewController: NSViewController {
+
+    @IBOutlet var skView: SKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let view = self.skView {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
+
